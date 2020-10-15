0cdfbced3858c3851c89166260b36bf63becb932
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-10-15 21:14:52 +0530

    postHandler

--- upload-frontend/src/App.js
+++ upload-frontend/src/App.js
@@ -11,6 +11,31 @@ class App extends Component {
     console.log(event.target.files[0]);
     this.setState({ imageUploaded: event.target.files[0] });
   };
+
+  postHandler = () => {
+    let fd = new FormData();
+    fd.append("image", this.state.imageUploaded);
+    // console.log(fd);
+    fetch("http://localhost:8080/cert", {
+      headers: {
+      }, method: "POST", body: fd,
+    })
+      .then((response) => {
+        // console.log(response.status);
+        if (response.status === 200) {
+          alert("Thank you for uploading the certificate!");
+        } else if (response.status === 400) {
+          alert("Please upload an image!");
+        } else if (response.status === 500) {
+          alert("Server error, please try again later!");
+        }
+      })
+      .catch((err) => {
+        // console.log(err);
+        return Promise.reject();
+      });
+  };
+
   render() {
     return (
       <div className="App">
