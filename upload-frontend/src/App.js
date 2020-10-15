a3477bbbac8d641fda92b630a0c5d0c261f6fcca
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-10-15 21:11:04 +0530

    display code

--- upload-frontend/src/App.js
+++ upload-frontend/src/App.js
@@ -5,7 +5,20 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />        
+        </header>
+        <p style={{fontSize: '20px'}}>Please upload your certificate in a png or jpeg format:</p>
+        <input
+          type="file"
+          id="image"
+          onChange={this.fileChangedHandler}
+        ></input>
+        <button onClick={() => this.postHandler()}>
+          Post
+        </button>
+      </div>      
     );
   }
   
